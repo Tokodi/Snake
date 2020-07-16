8dc51b85e74e21b98f4288d8b267c0da68455e69
András Tokodi <tokodiandrs@Andrass-MacBook-Pro.local>
2020-07-17 00:24:21 +0200

    Remove some leftovers...

--- ncurses_main.cc
+++ ncurses_main.cc
@@ -61,39 +61,4 @@ int main() {
     }
 
     return -1;
-
-
-
-
-
-
-
-
-
-
-
-//    std::shared_ptr<Snake::Model::Game> gameModel = std::make_shared<Snake::Model::Game>();
-//    gameModel->Initialize(40, 40);
-//
-//    Snake::UI::UINcurses ui(gameModel, 0, 0);
-//
-//    while (!gameModel->IsGameOver()) {
-//        ui.HandleInput();
-//        ui.Draw();
-//        gameModel->StepGame();
-//        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-//    }
-
-    //Snake::UI::MainWindow mainWindow;
-    //mainWindow.StartMenuLoop();
-
-//    Snake::UI::MenuWindow menuWindow(std::make_pair<int, int>(0, 0), 35, 15);
-//    menuWindow.Show();
-//    if (menuWindow.GetUserInput() == Snake::UI::StatusType::EXIT) {
-//        Snake::UI::Utils::Terminate();
-//        return -1;
-//    }
-
-
-    return 0;
 }
